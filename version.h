82bce4108f6e778eb9a22d3e1c903d68deca07d6
jsaez-amepower <84043347+jsaez-amepower@users.noreply.github.com>
2024-05-28 08:09:03 -0400

    Add version.h

---
+++ version.h
@@ -0,0 +1,11 @@
+#define VERSION_MAJOR      0 //based on git tag
+#define VERSION_MINOR      0 //based on git tag
+#define VERSION_PATCH      0 //based on the number of commits since the git tag was updated
+#define VERSION_BUILD      0 //based on the number of builds since a commit was made
+
+#define VERSION_BRANCH     "main"
+#define VERSION_COMMIT     0x0
+#define VERSION_COMMIT_SHORT     0x0
+#define VERSION_COMMIT_STR     "0"
+
+#define VERSION_STRING "0.0.0"
